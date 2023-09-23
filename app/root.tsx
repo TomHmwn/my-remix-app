59bd96b16383652c49f7bf870d396b2da8df12f8
Thomas Himawan <thimawan@student.unimelb.edu.au>
2023-09-23 12:28:48 +1000

    Remix tutorial

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,61 @@
+import { json, redirect } from "@remix-run/node";
+import { useEffect, useState } from "react";
+
 import {
   Form,
   Links,
   LiveReload,
   Meta,
+  NavLink,
+  Outlet,
+  useLoaderData,
   Scripts,
   ScrollRestoration,
+  useNavigation,
+  useSubmit,
 } from "@remix-run/react";
 
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+
+import appStylesHref from "./app.css";
+
+import { createEmptyContact, getContacts } from "./data";
+
+
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return json({ contacts, q });
+};
+
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
 export default function App() {
+  const { contacts, q } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+  const submit = useSubmit();
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has(
+      "q"
+    );
+
+
+  // the query now needs to be kept in state
+  const [query, setQuery] = useState(q || "");
+
+  // we still have a `useEffect` to synchronize the query
+  // to the component state on back/forward button clicks
+  useEffect(() => {
+    setQuery(q || "");
+  }, [q]);
+
+
   return (
     <html lang="en">
       <head>
@@ -20,30 +68,76 @@ export default function App() {
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+            <Form id="search-form" role="search"
+              onChange={(event) => {
+                const isFirstSearch = q === null;
+                submit(event.currentTarget, {
+                  replace: !isFirstSearch,
+                });
+              }}
+            >
               <input
                 id="q"
+                value={query}
+                className={searching ? "loading" : ""}
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
                 name="q"
+                // synchronize user's input to component state
+                onChange={(event) =>
+                  setQuery(event.currentTarget.value)
+                }
               />
-              <div id="search-spinner" aria-hidden hidden={true} />
+              <div id="search-spinner" aria-hidden hidden={!searching} />
             </Form>
             <Form method="post">
               <button type="submit">New</button>
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive
+                          ? "active"
+                          : isPending
+                            ? "pending"
+                            : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
+
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
+        </div>
+        <div id="detail"
+          className={
+            navigation.state === "loading" && !searching ? "loading" : ""
+          }
+        >
+          <Outlet />
         </div>
 
         <ScrollRestoration />
@@ -53,3 +147,8 @@ export default function App() {
     </html>
   );
 }
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+};
